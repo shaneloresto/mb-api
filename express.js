6498924cbc9f658d020ed62aab36d5ea323d3bf5
Shane Loresto <c0545151@camosun.ca>
2026-01-29 10:36:24 -0800

    added default path for API

--- express.js
+++ express.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import morgan from 'morgan';
 import cors from 'cors';
+import apiRouter from './routes/api-router.js';
 const app = express();
 // Express middleware
 app.use(express.json());
@@ -14,4 +15,5 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Node.js Server is live!');
 });
+app.use('/v1', apiRouter);
 export default app;
