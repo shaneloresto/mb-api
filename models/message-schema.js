d685bd1867bd8bba1256a4c25589db9270721d73
Shane Loresto <c0545151@camosun.ca>
2026-02-05 10:58:51 -0800

    finished task 3 lab 5

--- models/message-schema.js
+++ models/message-schema.js
@@ -1,11 +1,28 @@
-import z from 'zod';
-// Data Schema for a New Message
-// matches (mostly) the one from the front-end App!
-const messageSchema = z.object({
-    text: z
-        .string()
-        .trim()
-        .min(3)
-        .max(30)
+// import z from 'zod';
+// // Data Schema for a New Message
+// // matches (mostly) the one from the front-end App!
+// const messageSchema = z.object({
+//     text: z
+//         .string()
+//         .trim()
+//         .min(3)
+//         .max(30)
+// });
+// export default messageSchema;
+
+import mongoose from 'mongoose';
+const messageSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 30
+    }
 });
-export default messageSchema;
+messageSchema.set('toJSON', {
+    versionKey: false,
+    virtuals: true,
+    transform: (doc, ret) => { delete ret._id; }
+});
+export default mongoose.model('message', messageSchema);
