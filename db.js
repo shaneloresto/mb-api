a78e733028c41d8b88a365ed25b16f05e8e0e3d6
Shane Loresto <c0545151@camosun.ca>
2026-02-05 10:59:13 -0800

    added db.js

---
+++ db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+// connect to locally running MongoDB instance
+let dbURI = 'mongodb://localhost:27017/messages_db';
+mongoose.set('strictQuery', true);
+mongoose.connect(dbURI);
+// print message to console when connected to DB
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to ' + dbURI);
+});
+// print error message to console
+// if there is a problem connecting
+mongoose.connection.on('error', (err) => {
+    console.log('Mongoose connection error:' + err);
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose disconnected');
+});
+import './models/message-schema.js';
