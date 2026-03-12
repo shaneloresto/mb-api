3af2d82d27458dbeef30c6fca29efbf4b43e7d24
Shane Loresto <c0545151@camosun.ca>
2026-03-12 13:08:01 -0700

    added password hashing and salting to this project

--- models/user-schema.js
+++ models/user-schema.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import argon2 from 'argon2';
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -24,5 +25,15 @@ userSchema.set('toJSON', {
     versionKey: false,
     virtuals: true,
     transform: (doc, ret) => { delete ret._id; }
+});
+// Pre-Hook to Salt and Hash a password using argon2id
+userSchema.pre('save', async function() {
+    // hash and salt password
+    try {
+        const hash = await argon2.hash(this.password);
+        this.password = hash;
+    } catch (err) {
+        console.log('Error in hashing password' + err);
+    }
 });
 export default mongoose.model('user', userSchema);
