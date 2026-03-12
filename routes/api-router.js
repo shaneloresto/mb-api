8e74cf7f5212ddf055dcafd148af1382fbe254a3
Shane Loresto <c0545151@camosun.ca>
2026-03-12 13:18:14 -0700

    protected the post request for the /messages endpoint

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import msgAPIController from '../controllers/msg-api-controller.js';
-import userAPIController from '../controllers/user-api-controller.js'
+import userAPIController from '../controllers/user-api-controller.js';
+import passport from 'passport';
 const router = express.Router();
 router.route('/messages')
 .get(msgAPIController.getAllMessages)
-.post(msgAPIController.addNewMessage);
+.post(passport.authenticate('basic', { session: false }), msgAPIController.addNewMessage);
 router.route('/users')
 .post(userAPIController.registerNewUser);
 router.route('/messages/:id')
