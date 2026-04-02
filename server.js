31aa41bf7cc9695ac019cb02a4b42d96ec244b06
Shane Loresto <c0545151@camosun.ca>
2026-04-02 08:28:24 -0700

    everything working added socketio

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import app from './express.js';
+import {Server} from 'socket.io';
+import { handleSocket } from './socket.js';
 const port = process.env.PORT || 3004;
-app.listen(port, (err) => {
+const httpServer = app.listen(port, (err) => {
     if (err) console.log(err);
     console.info(`Server started on port ${port}.`);
 });
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*'
+  }
+});
+handleSocket(io);
---
+++ socket.js
@@ -0,0 +1,8 @@
+export const handleSocket = io => {
+  io.on('connection', socket => {
+    console.log('a user connected: ', socket.id);
+    socket.on('disconnect', () => {
+      console.log('user disconnected: ', socket.id);
+    });
+  });
+}
