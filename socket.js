353c9a623cca810e8000d1740c39b7f183cb09bc
Shane Loresto <c0545151@camosun.ca>
2026-04-02 08:31:03 -0700

    added chat messaging

--- socket.js
+++ socket.js
@@ -1,5 +1,9 @@
 export const handleSocket = io => {
   io.on('connection', socket => {
+    socket.on('chat message', message => {
+        console.log(`${socket.id}: ${message}`)
+        io.emit('chat message', message) 
+    });
     console.log('a user connected: ', socket.id);
     socket.on('disconnect', () => {
       console.log('user disconnected: ', socket.id);
